129488e96e1555c06643e1b34ef0ec4dacdb8f1d
Étienne Nadeau <1937092@etudiant.cegepvicto.ca>
2023-04-20 18:52:59 -0400

    modification des titres des fichiers dans repository vehicule

---
+++ .history/src/Domain/Vehicule/Repository/Vehicule/AfficherVehiculeRepository_20230420185146.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Domain\Vehicules\Repository\Vehicule;
+
+use PDO;
+ /// Repository.
+class AfficherVehiculeRepository
+{
+    /**
+     * @var PDO The database connection
+     */
+    private $connection;
+    
+     /// Constructor.
+    
+     /**
+     * @param PDO $connection The database connection
+     */
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function SelectToutVehicules(): array
+    {
+        $sql = "SELECT * FROM vehicule";
+
+        $query = $this->connection->prepare($sql);
+        $query->execute();
+
+        $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        return $resultat;
+    }
+
+    public function SelectVehiculeId(int $vehiculeId): array
+    {
+        $params = [
+            "id" => $vehiculeId
+        ];
+        $sql = "SELECT * FROM vehicule WHERE id = :id";
+
+        $query = $this->connection->prepare($sql);
+        $query->execute($params);
+        if ($query){
+            $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        else{
+            return [];
+        }
+
+        return $resultat;
+    }
+    
+}
+?>
